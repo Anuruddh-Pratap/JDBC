c311d803e567f399b8989377946e095373226c8a
Anuruddh-Pratap <90863157+Anuruddh-Pratap@users.noreply.github.com>
2021-12-06 12:49:25 +0530

    ArrayList

---
+++ src/MyEx12.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class MyEx12 {
+    public static void main(String[] args) {
+        ArrayList obj =new ArrayList();
+        obj.add("neeraj sir");
+        obj.add(10);
+        System.out.println(obj);
+    }
+}
+
